f330eb2ddc4737867c7002ee5469bbe161561d1a
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2023-12-12 15:14:43 +0100

    filter in frontend done

--- app_ui/app.py
+++ app_ui/app.py
@@ -12,7 +12,7 @@ st.set_page_config(initial_sidebar_state="collapsed")
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
 show_pages([
-    Page("app_ui/app.py","Search"),
+    Page("app_ui/app.py","app"),
     Page("app_ui/player_suggestion.py","player_suggestion")
 ])
 
@@ -29,20 +29,26 @@ if st.session_state.get('search_btn') != True:
     st.session_state['search_btn'] = search_btn
 
 if st.session_state['search_btn'] == True:
-    params = {'player_name': player_search}
-    response = requests.get(base_url_api + '/find_player_by_name', params=params)
-    players_search = response.json()
+    if player_search == '':
+        st.warning('Search Player by Name')
+    else:
+        params = {'player_name': player_search}
+        response = requests.get(base_url_api + '/find_player_by_name', params=params)
+        players_search = response.json()
 
-    st.markdown(""" ### Choose Player """)
-    cols = st.columns(5)
-    col_num = 0
-    for index, name in players_search['players'].items():
-        with cols[col_num]:
-            container = st.container(border=True)
-            container.write(name)
-            if container.button('Select', key=index):
-                st.session_state['search_btn'] = False
-                st.session_state['choosen_player'] = {'index': index, 'name': name}
-                switch_page('player_suggestion')
+        if len(players_search['players']) == 0:
+            st.markdown(''' No Players Found! ''')
+        else:
+            st.markdown(""" ### Choose Player """)
+            cols = st.columns(5)
+            col_num = 0
+            for player in players_search['players']:
+                with cols[col_num]:
+                    container = st.container(border=True)
+                    container.write(player['short_name'])
+                    if container.button('Select', key=player['idx']):
+                        st.session_state['search_btn'] = False
+                        st.session_state['choosen_player'] = player
+                        switch_page('player_suggestion')
 
-        col_num = 0 if col_num == 4 else col_num + 1
+                col_num = 0 if col_num == 4 else col_num + 1
